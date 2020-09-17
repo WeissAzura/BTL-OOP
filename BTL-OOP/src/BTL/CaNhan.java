3d145a3b3202708d24381f9d3d4db38de04f7818
WeissAzura <65398156+WeissAzura@users.noreply.github.com>
2020-09-17 18:56:16 +0700

    Class CaNhan

---
+++ BTL-OOP/src/BTL/CaNhan.java
@@ -0,0 +1,18 @@
+package BTL;
+
+public class CaNhan {
+	private String HoTen;
+	private String SDT;
+	private String DiaChi;
+	CaNhan() {
+		this.HoTen = "";
+		this.SDT = "";
+		this.DiaChi = "";
+	}
+	CaNhan(String a, String b, String c) {
+		this.HoTen = a;
+		this.SDT = b;
+		this.DiaChi = c;
+	}
+	
+}
